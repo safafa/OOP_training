05494b9933d332757d79522b15eacf85828e53f1
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-10-20 18:18:37 +0100

    Add method to get user input for rentals

--- main.rb
+++ main.rb
@@ -114,7 +114,27 @@ class App
     puts ''
   end
 
-  def create_rental; end
+  def create_rental
+    puts 'Select a book from the following list by number'
+    @books.each_with_index { |book, index| puts "#{index}) Title: #{book.title}, Author: #{book.author}" }
+
+    book_index = gets.chomp.to_i
+
+    puts 'Select a person from the following list by number (not id)'
+    @people.each_with_index do |person, index|
+      puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+
+    person_index = gets.chomp.to_i
+
+    print 'Date: '
+    date = gets.chomp
+
+    rental = Rental.new(date, @books[book_index], @people[person_index])
+
+    @rentals << rental
+    puts 'Rental created successfully'
+  end
 
   def list_rentals; end
 
