90fcf4892447231c8de7c775f537c61918eab4bb
unknown <safaaballagh97@gmail.com>
2021-10-20 12:34:51 +0100

    Add relationship book rental person

--- rental.rb
+++ rental.rb
@@ -1,7 +1,11 @@
 class Rental
   attr_accessor :date
 
-  def initialize(date)
+  def initialize(date, book, person)
     @date = date
+    @book = book
+    @book.rentals.push(self)
+    @person = person
+    @book.rentals.push(self)
   end
 end
