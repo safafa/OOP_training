1a774bc0c126d1c8c84977bb5bd040099228b620
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-10-20 15:44:45 +0100

    Add commands for each option entered

--- main.rb
+++ main.rb
@@ -1,7 +1,43 @@
 #!/usr/bin/env ruby
+require_relative 'book'
+require_relative 'classroom'
+require_relative 'person'
+require_relative 'rental'
+require_relative 'student'
+require_relative 'teacher'
+
 class App
   def run
-    puts 'Welcome to School Library App!'
+    puts "Welcome to School Library App!\n\n"
+    commands
+    command = gets.chomp
+    case command
+    when '1'
+      list_books
+    when '2'
+      list_people
+    when '3'
+      create_person
+    when '4'
+      create_book
+    when '5'
+      create_rental
+    when '6'
+      list_rentals
+    else
+      puts 'Thank you for using this app!'
+    end
+  end
+
+  def commands
+    puts 'Please choose an option by entering a number:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
   end
   
   def list_books; end
