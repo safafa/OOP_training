710fcd8073ab12ea9e6502967379aaaa13c80fcd
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-10-20 16:13:24 +0100

    Modify commands as a loop

--- main.rb
+++ main.rb
@@ -7,26 +7,38 @@ require_relative 'student'
 require_relative 'teacher'
 
 class App
+  def initialize
+    @books = []
+    @people = []
+    @rentals = []
+  end
+
   def run
     puts "Welcome to School Library App!\n\n"
-    commands
-    command = gets.chomp
-    case command
-    when '1'
-      list_books
-    when '2'
-      list_people
-    when '3'
-      create_person
-    when '4'
-      create_book
-    when '5'
-      create_rental
-    when '6'
-      list_rentals
-    else
-      puts 'Thank you for using this app!'
-    end
+    command = nil
+    while command != '7'
+      commands
+      
+      command = gets.chomp
+      case command
+      when '1'
+        list_books
+      when '2'
+        list_people
+      when '3'
+        create_person
+      when '4'
+        create_book
+      when '5'
+        create_rental
+      when '6'
+        list_rentals
+      when '7'
+        puts 'Thank you for using this app!'
+      else
+        puts 'Invalid option'
+      end
+    end    
   end
 
   def commands
@@ -46,7 +58,7 @@ class App
 
   def create_person; end
 
-  def create_book; end
+  def create_book;  end
 
   def create_rental; end
 
